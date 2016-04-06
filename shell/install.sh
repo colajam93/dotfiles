732855b3e52b08f49880fea7654ceaf4b90fbcbd
colajam93 <fs.fake.g1@gmail.com>
2016-04-06 21:23:01 +0900

    add Oh My Zsh installing

--- shell/install.sh
+++ shell/install.sh
@@ -6,3 +6,7 @@ dotfile_install $script_dir/bash_profile $HOME
 dotfile_install $script_dir/bashrc $HOME
 dotfile_install $script_dir/shell_common.sh $HOME
 dotfile_install $script_dir/zshrc $HOME
+if ! [[ -e $HOME/.oh-my-zsh ]]; then
+    _print_information "installing Oh My Zsh"
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
