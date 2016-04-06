0e72f6eb58df1e085778229b786b236cb40013f9
colajam93 <fs.fake.g1@gmail.com>
2016-04-07 01:46:37 +0900

    add python environment script

---
+++ python/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+check_command() {
+    if ! type $1 > /dev/null 2>&1; then
+        echo "missing $1"
+        exit 1
+    fi
+}
+
+check_command python
+check_command pip 
+check_command virtualenv
+source virtualenvwrapper_lazy.sh
+workon default
+
+pip install yapf
+
+deactivate
