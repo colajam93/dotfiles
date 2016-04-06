12592dbf9977791fc9d3a7911a73aea42b9bc5aa
colajam93 <fs.fake.g1@gmail.com>
2016-04-06 18:38:59 +0900

    add shell install script

---
+++ shell/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $script_dir/../lib/dotfiles.sh
+dotfile_install $script_dir/bash_profile $HOME
+dotfile_install $script_dir/bashrc $HOME
+dotfile_install $script_dir/shell_common.sh $HOME
+dotfile_install $script_dir/zshrc $HOME
