ca271190a00d185c479644913981dda17d7805f3
colajam93 <fs.fake.g1@gmail.com>
2016-04-06 19:26:30 +0900

    update vim install script

--- vim/install.sh
+++ vim/install.sh
@@ -6,4 +6,4 @@ source $script_dir/../lib/dotfiles.sh
 install -d $dein_config_dir
 safe_install $script_dir/plugins.toml $dein_config_dir
 safe_install $script_dir/plugins_lazy.toml $dein_config_dir
-safe_install $script_dir/vimrc $HOME/.vimrc
+dotfile_install $script_dir/vimrc $HOME
