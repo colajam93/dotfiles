d1625e90bd0de34709b372a0692f60fdcbfa8817
colajam93 <fs.fake.g1@gmail.com>
2016-06-05 03:32:16 +0900

    Update top install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,48 @@
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-exec_install() {
-    dir=$1
-    shift
-    bash "${script_dir}/${dir}/install.sh" $@
+exec_script() {
+    bash "${script_dir}/$1/install.sh"
 }
 
-exec_install misc $@
-exec_install shell $@
-exec_install vim $@
+exec_package() {
+    local e
+    for e in "${@}"; do
+        exec_script "$e"
+    done
+}
+
+package_default() {
+    local list=("core" "develop" "oh-my-zsh" "shell" "vim")
+    exec_package "${list[@]}"
+}
+
+package_simple() {
+    local list=("core" "shell" "vim-simple")
+    exec_package "${list[@]}"
+}
+
+package_all() {
+    local list=("core" "develop" "extra" "i3" "oh-my-zsh" "shell" "vim")
+    exec_package "${list[@]}"
+}
+
+
+# main
+
+# default: install default package
+if [[ "$#" == 0 ]]; then
+    target='default'
+else
+    target="$1"
+fi
+
+if [[ "${target}" == 'default' ]]; then
+    package_default
+elif [[ "${target}" == 'simple' ]]; then
+    package_simple
+elif [[ "${target}" == 'all' ]]; then
+    package_all
+else
+    exec_package "${target}"
+fi
