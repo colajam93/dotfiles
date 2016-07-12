90c2e19bd0907e37f2df34b39d01535597e6245e
colajam93 <fs.fake.g1@gmail.com>
2016-07-13 01:37:55 +0900

    Add testenvctl install

--- script/install.sh
+++ script/install.sh
@@ -6,3 +6,4 @@ source $script_dir/../lib/dotfiles.sh
 local_script_dir=$HOME/.local/bin
 install -d $local_script_dir
 safe_install $script_dir/vboxctl $local_script_dir 755
+safe_install $script_dir/testenvctl $local_script_dir 755
