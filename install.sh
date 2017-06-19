5a70db04169d51692a4309ddcb7281a1e4630d3e
colajam93 <fs.fake.g1@gmail.com>
2017-06-19 19:20:59 +0900

    Refactor main install script

--- install.sh
+++ install.sh
@@ -1,48 +1,29 @@
 #!/usr/bin/env bash
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-exec_script() {
-    bash "${script_dir}/$1/install.sh"
-}
+source $script_dir/lib/dotfiles.sh
 
 exec_package() {
     local e
     for e in "${@}"; do
-        exec_script "$e"
+        bash "${script_dir}/$e/install.sh"
     done
 }
-
-package_default() {
-    local list=("core" "develop" "oh-my-zsh" "script" "shell" "vim")
-    exec_package "${list[@]}"
-}
-
-package_simple() {
-    local list=("core" "shell" "vim-simple")
-    exec_package "${list[@]}"
-}
-
-package_all() {
-    local list=("core" "develop" "extra" "i3" "oh-my-zsh" "script" "shell" "vim")
-    exec_package "${list[@]}"
-}
-
 
 # main
 
 # parse options
-force=false
-quiet=false
+force="false"
+quiet="false"
 for OPT in "$@"
 do
     case "$OPT" in
         '-f'|'--force' )
-            force=true
+            force="true"
             shift 1
             ;;
         '-q'|'--quiet' )
-            quiet=true
+            quiet="true"
             shift 1
             ;;
         '--'|'-' )
@@ -63,25 +44,24 @@ do
     esac
 done
 
+if [[ -z ${param} ]] || [[ ${param[0]} == "default" ]]; then
+    targets=("core" "develop" "oh-my-zsh" "script" "shell" "vim")
+elif [[ ${param[0]} == "simple" ]]; then
+    targets=("core" "shell" "vim-simple")
+elif [[ ${param[0]} == "all" ]] && [[ $(platform_name) == "linux" ]]; then
+    targets=("core" "develop" "extra" "i3" "oh-my-zsh" "script" "shell" "vim")
+elif [[ ${param[0]} == "all" ]] && [[ $(platform_name) == "mac" ]]; then
+    targets=("brew" "core" "develop" "oh-my-zsh" "script" "shell" "vim")
+else
+    targets=("${param[@]}")
+fi
+
+# set global options
 export _dotfiles_force=$force
 export _dotfiles_quiet=$quiet
 
-# default: install default package
-if [[ -z ${param} ]]; then
-    target='default'
-else
-    target="${param[0]}"
-fi
+exec_package "${targets[@]}"
 
-if [[ "${target}" == 'default' ]]; then
-    package_default
-elif [[ "${target}" == 'simple' ]]; then
-    package_simple
-elif [[ "${target}" == 'all' ]]; then
-    package_all
-else
-    exec_package "${target}"
-fi
-
+# unset global options
 unset _dotfiles_force
 unset _dotfiles_quiet
