4927ed4e6872ad3f55d9fabce6d517f7606281d8
colajam93 <fs.fake.g1@gmail.com>
2017-07-23 16:49:49 +0900

    vim: Add Neovim config

--- vim/install.sh
+++ vim/install.sh
@@ -3,8 +3,16 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $script_dir/../lib/dotfiles.sh
 
+# Vim
 dein_config_dir=$HOME/.vim
 install -d $dein_config_dir
 safe_install $script_dir/plugins.toml $dein_config_dir
 safe_install $script_dir/plugins_lazy.toml $dein_config_dir
 install_all $script_dir plugins.toml plugins_lazy.toml
+
+# Neovim
+nvim_config_dir=$HOME/.config/nvim
+install -d $nvim_config_dir
+safe_install $script_dir/vimrc $nvim_config_dir/init.vim
+safe_install $script_dir/plugins.toml $nvim_config_dir
+safe_install $script_dir/plugins_lazy.toml $nvim_config_dir
