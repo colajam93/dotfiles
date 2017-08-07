26ac39b7605e419a683cf9f899c79f7d9aa5ae8b
colajam93 <fs.fake.g1@gmail.com>
2017-08-07 23:15:20 +0900

    Add latex templates

---
+++ latex/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $script_dir/../lib/dotfiles.sh
+
+latex_templates_dir=$HOME/.local/share/latex-templates
+local_script_dir=$HOME/.local/bin
+install -d $local_script_dir
+install -d $latex_templates_dir
+safe_install $script_dir/Makefile $latex_templates_dir
+safe_install $script_dir/mystylefile.sty $latex_templates_dir
+safe_install $script_dir/slide.tex $latex_templates_dir
+safe_install $script_dir/report.tex $latex_templates_dir
