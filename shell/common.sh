aa097b0797465efb740d7c0d3b77eeed7294d02c
colajam93 <fs.fake.g1@gmail.com>
2017-11-26 02:11:00 +0900

    shell: Update and remove functions
    man: Use latest ArchWiki's recipe.
    https://wiki.archlinux.org/index.php/Color_output_in_console#Using_less
    
    docker_cleanup: Removed. There is `docker system prune` so this function is not needed.

--- shell/common.sh
+++ shell/common.sh
@@ -1,14 +1,14 @@
 # function
 
 man() {
-    env LESS_TERMCAP_mb=$'\E[01;31m' \
-        LESS_TERMCAP_md=$'\E[01;38;5;74m' \
-        LESS_TERMCAP_me=$'\E[0m' \
-        LESS_TERMCAP_se=$'\E[0m' \
-        LESS_TERMCAP_so=$'\E[38;5;246m' \
-        LESS_TERMCAP_ue=$'\E[0m' \
-        LESS_TERMCAP_us=$'\E[04;38;5;146m' \
-        man "$@"
+    LESS_TERMCAP_mb=$'\E[01;31m' \
+    LESS_TERMCAP_md=$'\E[01;38;5;74m' \
+    LESS_TERMCAP_me=$'\E[0m' \
+    LESS_TERMCAP_se=$'\E[0m' \
+    LESS_TERMCAP_so=$'\E[38;5;246m' \
+    LESS_TERMCAP_ue=$'\E[0m' \
+    LESS_TERMCAP_us=$'\E[04;38;5;146m' \
+    command man "$@"
 }
 
 certinfo() {
@@ -17,18 +17,6 @@ certinfo() {
             return
     fi
     echo | openssl s_client -showcerts -servername $1 -connect $1:443 2>/dev/null | openssl x509 -noout -text
-}
-
-docker_cleanup() {
-    processes=$(docker ps -q -f status=exited)
-    if [ -n "$processes" ]; then
-      docker rm $processes
-    fi
-
-    images=$(docker images -q -f dangling=true)
-    if [ -n "$images" ]; then
-      docker rmi $images
-    fi
 }
 
 # alias
