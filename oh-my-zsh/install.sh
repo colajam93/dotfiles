3e5a4d6338e54887207365c7209e464d52c46017
colajam93 <fs.fake.g1@gmail.com>
2017-12-17 19:30:56 +0900

    zsh: Change Oh My Zsh path

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -3,7 +3,11 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $script_dir/../lib/dotfiles.sh
 
-if ! [[ -e $HOME/.oh-my-zsh ]]; then
+install_path=$HOME/.local/share/oh-my-zsh
+mkdir -p $HOME/.local/share
+if ! [[ -e $install_path ]]; then
     print_information "installing Oh My Zsh"
-    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    git clone git://github.com/robbyrussell/oh-my-zsh.git $install_path
+else
+    ZSH=$install_path sh $install_path/tools/upgrade.sh
 fi
