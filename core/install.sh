5d788dbd9cc66421232f1d4fb996c54ffdc644fc
colajam93 <fs.fake.g1@gmail.com>
2018-02-04 05:04:46 +0900

    core: Add global gitignore

--- core/install.sh
+++ core/install.sh
@@ -3,4 +3,6 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $script_dir/../external/libshellscript/libshellscript/libshellscript.sh
 
-install_all $script_dir
+install -d $HOME/.config/git
+safe_install $script_dir/gitignore_global $HOME/.config/git/ignore
+install_all $script_dir gitignore_global
