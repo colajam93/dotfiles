d7ac60d158b34572cb830ec07519a3c29f5962a9
colajam93 <fs.fake.g1@gmail.com>
2018-02-04 04:44:15 +0900

    scripts: Change repository path

--- script/install.sh
+++ script/install.sh
@@ -3,10 +3,12 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $script_dir/../external/libshellscript/libshellscript/libshellscript.sh
 
-install_path=$HOME/.local/share/scripts
+install_path=$HOME/.local/share/dotfiles/scripts
+repo_url='https://github.com/colajam93/scripts.git'
+install -d $(dirname $install_path)
 if ! [[ -e $install_path ]]; then
-    echo "Clone https://github.com/colajam93/scripts.git to $install_path"
-    git clone https://github.com/colajam93/scripts.git $install_path
+    echo "Clone $repo_url to $install_path"
+    git clone $repo_url $install_path
     cd $install_path
     bash install.sh
 else
