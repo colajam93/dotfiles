b4092b94127b5642ee020da3f0585d097160a24b
colajam93 <fs.fake.g1@gmail.com>
2019-06-29 18:57:13 +0900

    i3: Migrate to i3status-rust

--- i3/install.sh
+++ i3/install.sh
@@ -11,14 +11,4 @@ fi
 i3_dir=$HOME/.i3
 install -d $i3_dir
 safe_install $script_dir/config $i3_dir
-safe_install $script_dir/i3pystatus_config.py $i3_dir
-
-print_information "Prepare virtualenv for i3"
-source $(which virtualenvwrapper.sh)
-if ! lsvirtualenv -b | grep i3 > /dev/null 2>&1; then
-    mkvirtualenv i3
-else
-    workon i3
-fi
-print_information "Install i3 dependencies"
-pip install --upgrade -r "${script_dir}/requirements.txt"
+safe_install $script_dir/i3status-rust-config.toml $i3_dir
