799bd1c6a1e79706bafb71f160eab16ad64e42bd
colajam93 <fs.fake.g1@gmail.com>
2021-02-23 05:09:51 +0900

    Add Amazon ECR Docker Credential Helper config install

--- develop/install.sh
+++ develop/install.sh
@@ -6,4 +6,11 @@ source $script_dir/../external/libshellscript/libshellscript/libshellscript.sh
 k9s_dir=$HOME/.k9s
 install -d $k9s_dir
 safe_install $script_dir/k9s.yml $k9s_dir/config.yml
-install_all $script_dir k9s.yml
+
+if installed docker-credential-ecr-login; then
+    docker_dir=$HOME/.docker
+    install -d $docker_dir
+    safe_install $script_dir/docker.json $docker_dir/config.json
+fi
+
+install_all $script_dir k9s.yml docker.json
