782e661d36b8266c8d643dabc25e11f5ef50f914
colajam93 <fs.fake.g1@gmail.com>
2021-02-23 04:46:41 +0900

    develop: Add K9s config

--- develop/install.sh
+++ develop/install.sh
@@ -3,4 +3,7 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $script_dir/../external/libshellscript/libshellscript/libshellscript.sh
 
-install_all $script_dir
+k9s_dir=$HOME/.k9s
+install -d $k9s_dir
+safe_install $script_dir/k9s.yml $k9s_dir/config.yml
+install_all $script_dir k9s.yml
