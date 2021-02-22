b60198249a4000cbefcd49ecde3d43ebaeb00b7d
colajam93 <fs.fake.g1@gmail.com>
2021-02-23 04:02:13 +0900

    Remove LaTeX template installation

--- install.sh
+++ install.sh
@@ -49,9 +49,9 @@ if [[ -z ${param} ]] || [[ ${param[0]} == "default" ]]; then
 elif [[ ${param[0]} == "simple" ]]; then
     targets=("core" "shell" "vim-simple")
 elif [[ ${param[0]} == "all" ]] && [[ $(platform_name) == "linux" ]]; then
-    targets=("core" "develop" "oh-my-zsh" "script" "shell" "vim" "latex" "emacs")
+    targets=("core" "develop" "oh-my-zsh" "script" "shell" "vim" "emacs")
 elif [[ ${param[0]} == "all" ]] && [[ $(platform_name) == "mac" ]]; then
-    targets=("brew" "core" "develop" "oh-my-zsh" "script" "shell" "vim" "latex" "emacs")
+    targets=("brew" "core" "develop" "oh-my-zsh" "script" "shell" "vim" "emacs")
 else
     targets=("${param[@]}")
 fi
